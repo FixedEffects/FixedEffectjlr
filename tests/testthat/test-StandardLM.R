3de20e16a13375a83c9c17493350a041bdf14fd0
Erik Loualiche <erikloulou@gmail.com>
2018-03-29 20:08:49 -0400

    test

--- tests/testthat/test-StandardLM.R
+++ tests/testthat/test-StandardLM.R
@@ -5,16 +5,15 @@ df = Ecdat::Cigar
 test_that("run the Linear Model example", {
   skip_on_cran()
 
+  # JULIA_HOME = "/Applications/Julia-0.6.app/Contents/Resources/julia/bin"
   FixedEffect_setup()
 
   r <- FixedEffect_nse(df, sales~ndi, state+year, pop, cluster(state))
 
-  expect_named(r, c("results", "summary"), ignore.order = TRUE)
-  expect_equal(r$summary$mat,
+  expect_equal(names(r),
+               c("results", "summary"))
+  expect_equal(as.vector(r$summary$mat),
                c(-0.005262641, 0.001440434, -3.653511, 0.0002685935, -0.008088374, -0.002436907),
                tolerance = 0.001)
 
-  expect_equal(r$x, c(1.000, 4.743, 3.821, 1.379), tolerance = 0.001)
-
 })
-
